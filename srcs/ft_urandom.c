14df208ee64da01ee000570c7c8354086e35fa97
Your Name <bleplat@student.42.fr>
2019-03-05 21:12:16 +0100

    removed unnessessary include

--- srcs/ft_urandom.c
+++ srcs/ft_urandom.c
@@ -6,12 +6,11 @@
 /*   By: bleplat <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/05 14:55:05 by bleplat           #+#    #+#             */
-/*   Updated: 2018/12/05 15:17:03 by bleplat          ###   ########.fr       */
+/*   Updated: 2019/03/05 21:11:49 by bleplat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 unsigned int		ft_urandom(void)
 {
