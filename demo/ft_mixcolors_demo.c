5e0e849bdac73f1b7b6cfed13e24eda1196ce3e7
Your Name <bleplat@student.42.fr>
2019-03-16 20:12:53 +0100

    updated ft_mixcolors demo

--- demo/ft_mixcolors_demo.c
+++ demo/ft_mixcolors_demo.c
@@ -6,7 +6,7 @@
 /*   By: bleplat <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/16 19:55:14 by bleplat           #+#    #+#             */
-/*   Updated: 2019/03/16 20:05:00 by bleplat          ###   ########.fr       */
+/*   Updated: 2019/03/16 20:12:37 by bleplat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int				put_color(int r, int g, int b)
 	int		rgb;
 
 	rgb = ft_argb(0, r, g, b);
-	ft_printf("%Y R = %d, G = %d, B = %d\n", rgb, r, g, b);
+	ft_printf("%1$#Y   {}%1$Y R = %d, G = %d, B = %d\n", rgb, r, g, b);
 	return (rgb);
 }
 
@@ -35,6 +35,6 @@ int				main(int argc, char **argv)
 	c1 = put_color(ft_atoi(argv[1]), ft_atoi(argv[2]), ft_atoi(argv[3]));
 	c2 = put_color(ft_atoi(argv[4]), ft_atoi(argv[5]), ft_atoi(argv[6]));
 	c3 = ft_mixcolors(c1, c2, ft_atoi(argv[7]) / 100.0);
-	ft_printf("%YRESULT OF MIX %1$Y###\n", c3);
+	ft_printf("%1$#Y   {}%1$Y RESULT OF MIX\n", c3);
 	return (0);
 }
