9a7e58ee302adbb51b3c239d59b7721879582890
Your Name <bleplat@student.42.fr>
2020-03-01 17:31:50 +0100

    best display

--- upgrade_check.sh
+++ upgrade_check.sh
@@ -6,20 +6,23 @@
 #    By: bleplat <bleplat@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/03/01 17:05:03 by bleplat           #+#    #+#              #
-#    Updated: 2020/03/01 17:28:19 by bleplat          ###   ########.fr        #
+#    Updated: 2020/03/01 17:31:40 by bleplat          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 # Update
+printf "\e[35m"
 git remote update 1> /dev/null
 
 # Upgrade
 if [ -z "$(git status | grep 'branch is behind')" ]; then
 	printf "\e[35mUp to date.\e[0m\n"
 else
-	printf "\e[95m  / \\       \e[0m\n"
-	printf "\e[95m / | \\      \e[35mNew version available!\e[0m\n"
-	printf "\e[95m/  .  \\     \e[0m\n"
+	printf "\n"
+	printf "\e[95m     / \\                                      / \\   \e[0m\n"
+	printf "\e[95m    / | \\    \e[35mNEW VERSION AVAILABLE!    / | \\  \e[0m\n"
+	printf "\e[95m   /  .  \\                                  /  .  \\ \e[0m\n"
+	printf "\n"
 	read -p "Do you want to upgrade now? [y/N]" answer
 	if [ "$answer" = "y" ]; then
 		printf "\e[35mUpgrading...\e[0m\n"
